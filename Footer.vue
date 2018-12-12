f91cae20ed39dd9f7942e9b7b21e5e0055b1f02d
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2018-12-11 18:47:38 -0700

    Create Footer.vue

---
+++ Footer.vue
@@ -0,0 +1,32 @@
+template>
+  <header class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <a class="navbar-brand" href="/">PIZZA PLANET</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="">home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">menu</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+  header {
+    margin-bottom: 20px;
+  }
+
+  .navbar-brand {
+    font-size: 1.5em;
+  }
+</style>
