b8669d7057bd9a9812fee583bc85fae7da79d68f
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2018-12-14 11:48:02 -0700

    Create store.js

---
+++ store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//import * as actions from './actions'
+//import * as getters from './getters'
+//import * as mutations from './mutations'
+
+
+import menu from './modules/menu'
+import orders from './modules/orders'
+import users from './modules/users'
+import { firebaseMutations } from 'vuexfire'
+
+Vue.use(Vuex)
+
+
+export const store = new Vuex.Store({
+  mutations: { ...firebaseMutations },
+  modules: {
+    menu,
+    orders,
+    users
+ }
+})
